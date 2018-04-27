691e9c47875d6a8acdf99a2a91f769a600cb63a9
Linda Morales <lindagmorales94@gmail.com>
2018-04-27 14:43:12 -0400

    half done

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const html = require('html-template-tag');
+const { db } = require('./models');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ exrended: false }));
+app.use(bodyParser.json());
+app.use('/user', require('./routes/user'));
+app.use('/wiki', require('./routes/wiki'));
+
+db.authenticate().then(() => {
+  console.log('connected to the database');
+});
+
+app.get('/', (req, res, next) => {
+  res.send('hello world');
+});
+
+const syncDB = async () => {
+  await db.sync({ force: true });
+
+  const PORT = 3000;
+
+  app.listen(PORT, () => {
+    console.log("Server started. WOHOOOOO we're here!!! YAYYYYYY!!!!");
+  });
+};
+
+syncDB();
